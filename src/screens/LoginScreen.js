d2c84ddb3b0c6fd11b4a52ab8516763a838122bb
Woody Gelius <gelius24@hotmail.com>
2021-09-02 19:03:16 -0400

    ...

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -8,14 +8,17 @@ function LoginScreen() {
   return (
     <div className="loginScreen">
       <div className="loginScreen__background">
-        <img
+        <div className="loginScreen__header">
+          <img
           className="loginScreen__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button onClick={() => setSignIn(true)} className="loginScreen__button">
-          Sign In
-        </button>
+          <button onClick={() => setSignIn(true)} className="loginScreen__button">
+            Sign In
+          </button>
+        </div>
+        
         <div className="loginScreen__gradient" />
       </div>
 
